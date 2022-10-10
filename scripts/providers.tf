94cc27bfd201fc9b2355f0898de2d04553de3e31
akshatarora02 <akshatarora028@gmail.com>
2022-10-10 17:28:45 +1100

    fixed auth issues

--- scripts/providers.tf
+++ scripts/providers.tf
@@ -4,18 +4,12 @@ provider "google" {
     project = var.gcp_project
     region = var.location
 }
-data "google_client_config" "provider" {}
-
-data "google_container_cluster" "cluster" {
-  name     = var.app_name
-  location = var.location
-}
+data "google_client_config" "default" {}
 
 provider "kubernetes" {
-  host  = "https://${data.google_container_cluster.cluster.endpoint}"
-  token = data.google_client_config.provider.access_token
+  token = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(
-    data.google_container_cluster.cluster.master_auth[0].cluster_ca_certificate,
+    google_container_cluster.cluster.master_auth[0].cluster_ca_certificate,
   )
 }
 
