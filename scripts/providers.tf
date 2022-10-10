32bc164f0f355393ffdb12b33e63629e9a7c3315
akshatarora02 <akshatarora028@gmail.com>
2022-10-10 15:02:48 +1100

    uncommented k8s provider details

--- scripts/providers.tf
+++ scripts/providers.tf
@@ -12,11 +12,11 @@ data "google_client_config" "provider" {}
 #}
 
 provider "kubernetes" {
-  #host  = "https://${data.google_container_cluster.cluster.endpoint}"
+  host  = "https://${data.google_container_cluster.cluster.endpoint}"
   token = data.google_client_config.provider.access_token
-  #cluster_ca_certificate = base64decode(
-   # data.google_container_cluster.cluster.master_auth[0].cluster_ca_certificate,
-  #)
+  cluster_ca_certificate = base64decode(
+    data.google_container_cluster.cluster.master_auth[0].cluster_ca_certificate,
+  )
 }
 
 
