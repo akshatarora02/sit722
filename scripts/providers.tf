9c8f9da31280463e37166567bb05ecf810a543cc
akshatarora02 <akshatarora028@gmail.com>
2022-10-10 21:15:33 +1100

    reintroduced host

--- scripts/providers.tf
+++ scripts/providers.tf
@@ -7,6 +7,7 @@ provider "google" {
 data "google_client_config" "default" {}
 
 provider "kubernetes" {
+  host  = google_container_cluster.cluster.endpoint
   token = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(
     google_container_cluster.cluster.master_auth[0].cluster_ca_certificate,
