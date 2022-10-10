e1f94762dc017483482e01713482fcf1ee4dffb0
akshatarora02 <akshatarora028@gmail.com>
2022-10-10 14:59:35 +1100

    Added default to app version variable

--- scripts/variables.tf
+++ scripts/variables.tf
@@ -13,28 +13,13 @@ variable admin_username {
   default = "linux_admin"
 }
 
-variable "gcp_auth_file" {
-  type        = string
-  description = "GCP authentication file"
-}
-
 variable "bucket-name" {
   type        = string
   description = "The name of the Google Storage Bucket to create"
   default = "flixtubeakshatsit722"
 }
-variable "storage-class" {
-  type        = string
-  description = "The storage class of the Storage Bucket to create"
+
+variable app_version { 
+  default = 1
 }
 
-variable app_version { # Can't be called version! That's a reserved word.
-}
-
-variable client_id {
-
-}
-
-variable client_secret {
-
-}
